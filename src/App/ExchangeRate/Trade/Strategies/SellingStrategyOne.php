34e57facecbae20ec3b973402e79c386c9619bd3
Grzegorz Marszał <grzegorz@grzesiowo.pl>
2024-10-30 20:45:29 +0100

    Removed unecessary comments

--- src/App/ExchangeRate/Trade/Strategies/SellingStrategyOne.php
+++ src/App/ExchangeRate/Trade/Strategies/SellingStrategyOne.php
@@ -7,10 +7,6 @@ namespace App\ExchangeRate\Trade\Strategies;
 use App\ExchangeRate\DTO\ExchangeRateInterface;
 use App\ExchangeRate\Trade\CurrencyTradeRateCalculationInterface;
 
-/**
- * Nie mialem pomyslu na nazewnictwo w tym przypadku, pewnie jakbym byl blizej biznesu i konkretnej domeny to wiedzialbym
- * ze np. te  -0.07 etc. dla EUR i USD i innych walut ma jakies okreslenie
- */
 class SellingStrategyOne implements CurrencyTradeRateCalculationInterface
 {
     public function calculate(ExchangeRateInterface $rate): ?float
