b979215c6cf7de79e31f13f80030a5a47a4958c3
Mateusz Malecki <mateusz.malecki@dotfusion.eu>
2024-11-27 22:16:34 +0100

    adds throws tag

--- src/App/Repository/CurrencyValueRepositoryInterface.php
+++ src/App/Repository/CurrencyValueRepositoryInterface.php
@@ -2,10 +2,12 @@
 
 namespace App\Repository;
 
+use App\Exception\CurrencyValueNotFoundException;
 use App\Model\CurrencyValue;
 use DateTime;
 
 interface CurrencyValueRepositoryInterface
 {
+    /** @throws CurrencyValueNotFoundException */
     public function findByCurrencyCodeAndDate(string $code, DateTime $date): CurrencyValue;
 }
